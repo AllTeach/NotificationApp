1a5c08e1e50284fffffdb6aae5ac960670f67704
AllTeach <33581448+AllTeach@users.noreply.github.com>
2021-10-22 10:42:44 +0300

    Simple Notification

---
+++ app/src/main/java/com/example/notificationapp/App.java
@@ -0,0 +1,46 @@
+package com.example.notificationapp;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class App extends android.app.Application{
+
+    public static final String CHANNEL_1_ID = "channel1";
+    public static final String CHANNEL_2_ID = "channel2";
+    // starts before we start the activity
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createNotificationChannels();
+    }
+
+    private void createNotificationChannels()
+    {
+        // check if higher than Oreo
+        if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.O)
+        {
+            NotificationChannel channel1 = new NotificationChannel(CHANNEL_1_ID,
+                    "channel 1",
+                    NotificationManager.IMPORTANCE_HIGH);
+            // create the settings as default user can change
+            // user can see in the settigs
+
+            channel1.setDescription(" Channel 1 user for...");
+            NotificationChannel channel2 = new NotificationChannel(CHANNEL_2_ID,
+                    "channel 2",
+                    NotificationManager.IMPORTANCE_LOW);
+            // create the settings as default user can change
+            // user can see in the settigs
+
+            channel2.setDescription(" Channel 2 user for...");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+
+            manager.createNotificationChannel(channel1);
+            manager.createNotificationChannel(channel2);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/notificationapp/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.notificationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    // wraps bacground compatibiltiy
+    // cannot create channels
+    private NotificationManagerCompat notificationManagerCompat;
+    private EditText etTitle,etMessage;
+    private int counter=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notificationManagerCompat =  NotificationManagerCompat.from(this);
+        initViews();
+    }
+
+    private void initViews() {
+        etMessage = findViewById(R.id.editTextMessage);
+        etTitle = findViewById(R.id.editTextTitle);
+    }
+
+    public void Channel1(View view) {
+        counter++;
+        String title = etTitle.getText().toString();
+        String message = etMessage.getText().toString();
+
+        Intent intent = new Intent(this,MainActivity.class);
+        //PendingIntent pendingIntent = new
+
+        Notification notification = new NotificationCompat.Builder(this,App.CHANNEL_1_ID).
+                setSmallIcon(R.drawable.one)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setColor(Color.BLUE)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .build();
+
+        // id stands for showing multiple notifications
+        // at the same time
+        // if we use the same ID it will override the
+        // existing notification
+        notificationManagerCompat.notify(counter,notification);
+
+
+    }
+
+    public void Channel2(View view) {
+        counter++;
+        String title = etTitle.getText().toString();
+        String message = etMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this,App.CHANNEL_2_ID).
+                setSmallIcon(R.drawable.two)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .build();
+
+        // id stands for showing multiple notifications
+        // at the same time
+        // if we use the same ID it will override the
+        // existing notification
+        notificationManagerCompat.notify(counter,notification);
+
+    }
+}
